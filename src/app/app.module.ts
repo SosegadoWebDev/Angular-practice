cfb4b2cbbc10ea4960d716c119d53a0ff8f107ee
Jaime R. Gonzalez M <arcarius11@gmail.com>
2019-09-13 20:34:09 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WarningAlertComponent } from './WarningAlert/warning-alert.component';
+import { SuccessComponent } from './successAlert/success.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarningAlertComponent,
+    SuccessComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/successAlert/success.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core"
+
+@Component({
+    selector: "app-successAlert",
+    templateUrl: "./success.component.html",
+    styles: [`
+        span {
+            color: green;
+        }
+    `]
+})
+export class SuccessComponent {
+
+}
